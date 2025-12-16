33b8cc66201c0858351aac8991c55b3a92c74ec1
Sirraide <aeternalmail@gmail.com>
2025-12-16 04:33:28 +0100

    WIP

--- src/main.cc
+++ src/main.cc
@@ -1,40 +1,70 @@
-#include <algorithm>
+#include <base/Base.hh>
+#include <base/FS.hh>
+#include <base/Serialisation.hh>
+#include <base/Text.hh>
 #include <clopts.hh>
-#include <codecvt>
-#include <fmt/format.h>
-#include <fmt/xchar.h>
-#include <locale>
+#include <print>
 #include <random>
-#include <ranges>
 #include <regex>
-#include <unordered_map>
-#include <vector>
 
-namespace rgs = std::ranges;
-using c32 = char32_t;
+using namespace base;
 
-void to_lower(auto& str) {
-    rgs::transform(str, str.begin(), [](auto c) { return std::tolower(c); });
-}
-void to_upper(auto& str) {
-    rgs::transform(str, str.begin(), [](auto c) { return std::toupper(c); });
-}
+#ifdef USE_32_BIT_CHAIN
+using Character = char32_t;
+using String = std::u32string;
+#else
+using Character = char;
+using String = std::string;
+#endif
 
-auto to_utf8(const auto& str) -> std::string {
-    std::wstring_convert<std::codecvt_utf8<c32>, c32> conv;
-    return conv.to_bytes(str);
-}
+class Timer {
+    chr::milliseconds duration;
+    chr::steady_clock::time_point start;
 
-auto to_utf32(const auto& str) -> std::u32string {
-    std::wstring_convert<std::codecvt_utf8<c32>, c32> conv;
-    return conv.from_bytes(str);
-}
+public:
+    /// Create a new timer with the given duration and start it.
+    explicit Timer(chr::milliseconds duration)
+        : duration(duration), start(chr::steady_clock::now()) {}
+
+    /// Get the elapsed time normalised between 0 and 1.
+    [[nodiscard]] auto dt() const -> f64 { return dt(duration); }
+    [[nodiscard]] auto dt(chr::milliseconds d) const -> f64 {
+        return f64(elapsed().count()) / f64(d.count());
+    }
+
+    /// Get the time that has elapsed since the animation started.
+    [[nodiscard]] auto elapsed() const -> chr::milliseconds {
+        return chr::duration_cast<chr::milliseconds>(chr::steady_clock::now() - start);
+    }
+
+    /// Check if the timer has expired.
+    [[nodiscard]] auto expired() const -> bool {
+        return elapsed() >= duration;
+    }
+
+    /// Extend the duration of the current iteration of the timer.
+    void extend(chr::milliseconds extra) { start += extra; }
+
+    /// Restart the timer.
+    void restart() { start = chr::steady_clock::now(); }
+};
+
+class ProfileTimer : public Timer {
+public:
+    std::string scope;
+    ProfileTimer(std::string scope, chr::milliseconds duration = 0ms)
+        : Timer(duration), scope(std::move(scope)) {}
+
+    ~ProfileTimer() {
+        std::println(stderr, "[{}]: {} elapsed", scope, elapsed());
+    }
+};
 
 /// Split a string into lines.
 auto split_lines(std::string_view str) -> std::vector<std::string_view> {
     std::vector<std::string_view> ret;
-    size_t start = 0;
-    for (size_t i = 0; i < str.size(); i++) {
+    usz start = 0;
+    for (usz i = 0; i < str.size(); i++) {
         if (str[i] == '\n') {
             ret.push_back(str.substr(start, i - start));
             start = i + 1;
@@ -46,62 +76,101 @@ auto split_lines(std::string_view str) -> std::vector<std::string_view> {
 
 /// Trim a string.
 auto trim(std::string_view str) -> std::string_view {
-    size_t start = 0;
-    while (start < str.size() and std::isspace(str[start])) start++;
-    size_t end = str.size();
-    while (end > start and std::isspace(str[end - 1])) end--;
+    usz start = 0;
+    while (start < str.size() and text::IsSpace(str[start])) start++;
+    usz end = str.size();
+    while (end > start and text::IsSpace(str[end - 1])) end--;
     return str.substr(start, end - start);
 }
 
 /// Split a string by a regular expression.
-auto split(const std::string& str, const std::string& re) -> std::vector<std::string> {
-    std::vector<std::string> ret;
-    std::regex rex(re);
-    std::sregex_token_iterator iter(str.begin(), str.end(), rex, {-1, 0});
-    std::sregex_token_iterator end;
-    while (iter != end) {
-        ret.push_back(*iter);
-        iter++;
+auto split(str s, std::string_view re) -> std::vector<str> {
+    std::vector<str> ret;
+    regex rex(re);
+    while (not s.empty()) {
+        auto f = s.find(rex);
+        if (not f.has_value()) {
+            ret.push_back(s);
+            return ret;
+        }
+
+        ret.push_back(s.take(f->start));
+        s.drop(f->size());
     }
     return ret;
 }
 
-template <typename tchar_t = char>
 struct markov_chain {
-    using tchar = tchar_t;
-    using tstring = std::basic_string<tchar>;
+    using char_type = Character;
+    using text_type = std::basic_string_view<char_type>;
+    using string_type = std::basic_string<char_type>;
+    LIBBASE_SERIALISE(markov_chain, chain, order);
 
-    size_t order;
-    std::unordered_map<tstring, std::vector<tchar>> chain;
+    /// Map from ngrams to [char, frequency] pairs.
+    using map_type = std::unordered_map<string_type, std::unordered_map<char, u32>>;
+
+    map_type chain;
+    usz order;
+    usz seed;
     std::mt19937 rng;
-    size_t seed;
 
-    markov_chain(const tstring& text, size_t order, size_t _seed = std::random_device()()) : order(order), seed(_seed) {
-        for (size_t i = 0; i < text.size() - order; i++)
-            chain[text.substr(i, order)].push_back(text[i + order]);
+    markov_chain(text_type text, usz order, usz _seed = std::random_device()()) : order(order), seed(_seed) {
+        ProfileTimer _{"build"};
+        for (usz i = 0; i < text.size() - order; i++)
+            chain[map_type::key_type{text.substr(i, order)}][text[i + order]]++;
         rng.seed(seed);
     }
 
-    tstring generate(size_t length) {
-        tstring result;
+    markov_chain(map_type chain, usz order, usz seed = std::random_device()())
+        : chain(std::move(chain)), order(order), seed(seed) {
+        rng.seed(seed);
+    }
+
+    string_type generate(usz length) {
+        string_type result, ngram;
         result.reserve(length);
 
-        tstring ngram;
+        // Pick an ngram that starts with a space.
         for (;;) {
             auto next = chain.begin();
             std::advance(next, rng() % chain.size());
             ngram = next->first;
             if (!ngram.starts_with(' ')) continue;
-
             result.append(next->first);
             break;
         }
 
-        for (size_t i = 0; i < length; i++) {
+        // Append to it.
+        for (usz iterations = 0; iterations < length; iterations++) {
+            // Find the entry for the ngram in the chain; if there is no character
+            // that can follow this ngram, give up.
             auto it = chain.find(ngram);
             if (it == chain.end()) break;
-            result += it->second[rng() % it->second.size()];
-            ngram = result.substr(i + 1, order);
+
+            // Otherwise, randomly pick a character; we need to take probabilities
+            // into account here, e.g. if 'Y' occurs 10 times more often after 'X'
+            // than 'Z', then we should pick 'Y' 10/11 times.
+            //
+            // To implement this, pick an ‘index’ randomly and then find the element
+            // at that ‘index’; e.g. if our map is {{'a', 10}, {'b', 5}, {'c', '20'}},
+            // then we pick an index I between 1 and 35, and the next character will
+            // be 'a' if I is in 0..<10, 'b' if it is in '10..<15', and 'c' otherwise.
+            auto count = rgs::fold_left(it->second, 0zu, [](auto a, auto& pair) { return a + pair.second; });
+            auto i = rng() % count;
+            auto n = 0zu;
+            bool appended = false;
+            for (auto [c, p] : it->second) {
+                if (i < n + p) {
+                    appended = true;
+                    result += c;
+                    break;
+                }
+
+                n += p;
+            }
+
+            Assert(appended);
+            ngram = result.substr(iterations + 1, order);
         }
 
         return result;
@@ -118,91 +187,156 @@ using options = clopts< // clang-format off
     option<"--seed", "The seed for the random number generator", int64_t>,
     option<"--min-line", "Ignore lines that are shorter than this", int64_t>,
     option<"--split", "Split output by regex">,
+    option<"--save-chain", "Save the chain to a file">,
+    option<"--load-chain", "Load the chain from a file", file<std::vector<std::byte>>>,
     flag<"--dump-input", "Print the processed text instead of generating output">,
     flag<"--print-seed", "Print the seed used for the random number generator">,
     flag<"--ascii", "Strip non-ascii characters">,
+    flag<"--skip-preprocess", "Do not preprocess the input">,
     help<>
 >; // clang-format on
 
-void generate(std::string input) {
-    size_t length = options::get_or<"--length">(100);
-    size_t lines = options::get_or<"--lines">(1);
-    size_t order = options::get_or<"--order">(6);
+auto clean_up_input(options::optvals_type& opts, str input) -> String {
+    ProfileTimer _{"cleanup"};
+    std::string cleaned_up;
 
-    /// Remove short lines to avoid generating gibberish.
-    if (auto min_line = options::get<"--min-line">()) {
-        auto input_lines = split_lines(input);
-        std::erase_if(input_lines, [min = size_t(*min_line)](const auto& line) { return line.empty() || line.size() < min; });
-        input = fmt::format("{}", fmt::join(input_lines, "\n"));
+    // Remove short lines to avoid generating gibberish.
+    if (auto min_line = opts.get<"--min-line">()) {
+        ProfileTimer _{"cleanup: short lines"};
+        cleaned_up = input
+            | vws::split('\n')
+            | vws::filter([min = *min_line](auto&& r) { return rgs::distance(r) >= min; })
+            | vws::join_with(' ')
+            | rgs::to<std::string>();
+    } else {
+        ProfileTimer _{"cleanup: fold ws"};
+        cleaned_up = input.fold_ws();
     }
 
-    /// Replace newlines with spaces.
-    char* c = input.data();
-    while (c = strchr(c, '\n'), c) *c = ' ';
 
-    /// Remove non-ascii chars.
-    if (options::get<"--ascii">()) {
-        static const std::string ascii_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'\".,-_:;!?() ";
-        std::erase_if(input, [](char c) { return !ascii_chars.contains(c); });
+    // Remove non-ascii chars.
+    if (opts.get<"--ascii">()) {
+        ProfileTimer _{"cleanup: ascii"};
+        std::erase_if(cleaned_up, [](char c) { return not text::IsPrint(c); });
     }
 
-    /// Convert to lowercase.
-    to_lower(input);
+    // Convert to lowercase.
+    {
+        ProfileTimer _{"cleanup: lowercase"};
+        cleaned_up = text::ToLower(cleaned_up);
+    }
 
-    /// Print the input if requested.
-    if (options::get<"--dump-input">()) {
-        fmt::print("{}\n", input);
+#ifdef USE_32_BIT_CHAIN
+    // Convert to utf32.
+    ProfileTimer _{"cleanup: utf8->utf32"};
+    return text::ToUTF32(cleaned_up);
+#else
+    return cleaned_up;
+#endif
+}
+
+void generate(options::optvals_type& opts, markov_chain& mc) {
+    usz length = usz(opts.get<"--length">(100));
+    usz lines = usz(opts.get<"--lines">(1));
+
+    // Save the chain.
+    if (auto* path = opts.get<"--save-chain">()) {
+        File::Write(*path, ser::Serialise<std::endian::native>(mc)).value();
         return;
     }
 
-    /// Convert to utf32.
-    auto str = to_utf32(input);
+    // Print the seed.
+    if (opts.get<"--print-seed">()) std::println(stderr, "Seed: {}", mc.seed);
 
-    /// Build the markov chain.
-    markov_chain<c32> mc(str, order, size_t(options::get_or<"--seed">(std::random_device()())));
+    // Generate words.
+    ProfileTimer _{"generate"};
+    for (usz i = 0; i < lines; i++) {
+#ifdef USE_32_BIT_CHAIN
+        auto out = text::ToUTF8(mc.generate(length));
+#else
+        auto out = mc.generate(length);
+#endif
 
-    /// Print the seed.
-    if (options::get<"--print-seed">()) fmt::print("Seed: {}\n", mc.seed);
-
-    /// Generate words.
-    for (size_t i = 0; i < lines; i++) {
-        auto out = to_utf8(mc.generate(length));
-
-        /// Split the output if requested.
-        if (auto s = options::get<"--split">()) {
+        // Split the output if requested.
+        if (auto s = opts.get<"--split">()) {
             bool first = true;
             for (auto& line : split(out, *s)) {
                 if (first) first = false;
-                else if (line.size() > 5) fmt::print("\n");
-                fmt::print("{}", trim(line));
+                else if (line.size() > 5) std::println();
+                std::print("{}", trim(line));
             }
-            fmt::print("\n");
+            std::println();
+        } else {
+            std::println("{}", trim(out));
         }
-
-        else fmt::print("{}\n", trim(out));
     }
+}
+
+void build_chain_and_generate(options::optvals_type& opts, str input) {
+    usz order = usz(opts.get<"--order">(6));
+    String cleaned_up = opts.get<"--skip-preprocess">()
+#ifdef USE_32_BIT_CHAIN
+        ? text::ToUTF32(input)
+#else
+        ? String(input)
+#endif
+        : clean_up_input(opts, input);
+
+    // Print the input if requested.
+    if (opts.get<"--dump-input">()) {
+        std::println("{}", cleaned_up);
+        return;
+    }
+
+    // Build the markov chain.
+    markov_chain mc(cleaned_up, order, usz(opts.get<"--seed">(std::random_device()())));
+    generate(opts, mc);
 }
 
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "");
-    options::parse(argc, argv);
+    auto opts = options::parse(argc, argv);
+    if (auto* chain_file = opts.get<"--load-chain">()) {
+        Timer t{0ms};
+        markov_chain mc{ser::Deserialise<markov_chain, std::endian::native>(chain_file->contents).value()};
+        markov_chain::map_type m;
+        int x = 0;
+        for (auto p : mc.chain) {
+            m.insert(std::move(p));
+            if (x++ == 1000) break;
+        }
 
-    if (options::get<"--stdin">()) {
+        std::println("{}", m);
+        _Exit(41);
+        //std::unordered_map<int, int> x;
+        //for (const auto& [k, v] : mc.chain) x[v.size()]++;
+        //for (const auto& [k, v] : x) std::println("{} : {}", k, v);
+        //std::fflush(stdout);
+        //_Exit(42);
+
+
+        std::println(stderr, "[deserialisation] {}", t.elapsed());
+        generate(opts, mc);
+        return 0;
+    }
+
+    if (opts.get<"--stdin">()) {
         std::string input;
         std::string line;
         while (std::getline(std::cin, line)) {
             input += line;
             input += '\n';
         }
-        generate(std::move(input));
+
+        build_chain_and_generate(opts, input);
         return 0;
     }
 
-    auto fs = options::get<"-f">();
-    if (fs->empty()) {
-        fmt::print(stderr, "{}", options::help());
+    auto fs = opts.get<"-f">();
+    if (fs.empty()) {
+        std::print(stderr, "{}", options::help());
         return 1;
     }
 
-    for (auto& input : *fs) generate(std::move(input.contents));
+    for (auto& input : fs) build_chain_and_generate(opts, input.contents);
 }
