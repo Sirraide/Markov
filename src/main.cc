b94a0a098bbf539f3f7072d171c1504f20ae0c41
Sirraide <aeternalmail@gmail.com>
2025-12-16 18:45:39 +0100

    Massively improve the actualy generation part

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <base/Base.hh>
+#include <base/Compression.hh>
 #include <base/FS.hh>
 #include <base/Serialisation.hh>
 #include <base/Text.hh>
@@ -6,6 +7,7 @@
 #include <print>
 #include <random>
 #include <regex>
+#include <csignal>
 
 using namespace base;
 
@@ -16,6 +18,9 @@ using String = std::u32string;
 using Character = char;
 using String = std::string;
 #endif
+
+using Reader = ser::Reader<std::endian::native>;
+using Writer = ser::Writer<std::endian::native>;
 
 class Timer {
     chr::milliseconds duration;
@@ -102,13 +107,148 @@ auto split(str s, std::string_view re) -> std::vector<str> {
 
 //#define FREE_NODES
 
+struct header {
+    static constexpr u32 CurrentVersion = 1;
+
+    /// File format version.
+    u32 version = CurrentVersion;
+
+    /// Order of the chain.
+    u16 order;
+
+    /// Unused.
+    u16 _reserved;
+
+    /// Number of ngrams in the ngram table.
+    u64 num_ngrams;
+};
+
+struct markov_chain {
+    using ngram_type = __int128;
+    using offset_type = u64;
+    using index_type = u32;
+    using freq_size_type = u32;
+    using char_type = Character;
+    using string_type = std::basic_string<char_type>;
+
+    // Packed so we don’t have to worry about the serialised pointer being unaligned.
+    struct [[gnu::packed]] saved_ngram {
+        /// Value of the ngram.
+        ngram_type ngram;
+
+        /// Offset in bytes relative to the start of the frequency table.
+        offset_type freq_pairs_list_offset;
+    };
+
+    struct [[gnu::packed]] freq_table_entry {
+        freq_size_type freq;
+        char_type c;
+    };
+
+    struct [[gnu::packed]] freq_table {
+        freq_size_type num_elements;
+        freq_table_entry entry_data[];
+        auto elements() const -> Span<freq_table_entry> {
+            return {entry_data, num_elements};
+        }
+    };
+
+    ByteSpan data;
+    header hdr{};
+    std::mt19937 rng;
+    usz seed;
+
+    explicit markov_chain(ByteSpan data, usz seed) : data{data}, seed{seed} {
+        std::memcpy(&hdr, data.data(), sizeof(header));
+        Assert(hdr.version == header::CurrentVersion, "Chain is out of date; please regenerate");
+        rng.seed(seed);
+    }
+
+    auto all_ngrams() const -> Span<saved_ngram> {
+        return {
+            reinterpret_cast<const saved_ngram*>(data.data() + sizeof(header)),
+            hdr.num_ngrams
+        };
+    }
+
+    /// Find the entry for the ngram in the chain; if there is no character
+    /// that can follow this ngram, give up.
+    auto find_ngram(ngram_type ngram) const -> std::optional<saved_ngram> {
+        auto all = all_ngrams();
+        auto n = rgs::lower_bound(all, ngram, rgs::less(), &saved_ngram::ngram);
+        if (n == all.end()) return std::nullopt;
+        return *n;
+    }
+
+    /// Randomly pick a character; we need to take probabilities into account here,
+    /// e.g. if 'Y' occurs 10 times more often after 'X' than 'Z', then we should
+    /// pick 'Y' 10/11 times.
+    ///
+    /// To implement this, pick an ‘index’ randomly and then find the element
+    /// at that ‘index’; e.g. if our map is {{'a', 10}, {'b', 5}, {'c', '20'}},
+    /// then we pick an index I between 1 and 35, and the next character will
+    /// be 'a' if I is in 0..<10, 'b' if it is in '10..<15', and 'c' otherwise.
+    auto ngram_get_random_char(const saved_ngram& n) -> char_type {
+        auto table_offs = data.data() + sizeof(hdr) + hdr.num_ngrams * sizeof(saved_ngram) + n.freq_pairs_list_offset;
+        auto table = reinterpret_cast<const freq_table*>(table_offs);
+        auto i = rng() % table->num_elements;
+        freq_size_type sum = 0;
+        for (auto e : table->elements()) {
+            sum += e.freq;
+            if (i <= sum) return e.c;
+        }
+        Unreachable();
+    }
+
+    void dump() {
+        std::println("header");
+        std::println("  chain size: {}", utils::HumanReadable(data.size_bytes()));
+        std::println("  version: {}", hdr.version);
+        std::println("  ngrams: {}", hdr.num_ngrams);
+        std::println("  order: {}", hdr.order);
+        std::println("ngrams");
+        for (auto n : all_ngrams()) {
+            std::string s;
+            s.resize(hdr.order);
+            std::memcpy(s.data(), &n.ngram, hdr.order);
+            std::println("  - '{}' at {}", s, n.freq_pairs_list_offset);
+        }
+    }
+
+    string_type generate(usz length) {
+        ngram_type ngram;
+        string_type result;
+        result.reserve(length);
+
+        // Pick a random ngram to start with.
+        auto all = all_ngrams();
+        auto start = rng() % hdr.num_ngrams;
+        auto saved = all[start];
+        result.resize(hdr.order);
+        std::memcpy(result.data(), &saved.ngram, hdr.order);
+        ngram = saved.ngram;
+
+        // Append to it.
+        for (usz iterations = 0; iterations < length; iterations++) {
+            auto n = find_ngram(ngram);
+            if (not n.has_value()) break;
+            result += ngram_get_random_char(*n);
+            std::memcpy(&ngram, result.data() + iterations + 1, hdr.order);
+        }
+
+        return result;
+    }
+};
+
 struct node {
-    using large_t = std::flat_map<char, u32>;
+    using char_type = markov_chain::char_type;
+    using freq_type = markov_chain::freq_size_type;
+    using large_t = std::flat_map<char_type, freq_type>;
     uptr data : 63 = 0;
     uptr small : 1 = true;
 
     node() = default;
-    node(char c, u32 n) { inc_small_count(c, n); }
+    node(char_type c, freq_type n) { inc_small_count(c, n); }
     node(large_t l) {
         small = false;
         data = reinterpret_cast<uptr>(new large_t(std::move(l)));
@@ -137,7 +277,7 @@ struct node {
     }
 #endif // FREE_NODES
 
-    void add(char c) {
+    void add(char_type c) {
         if (small) {
             if (data == 0 or small_char() == c) {
                 inc_small_count(c);
@@ -156,9 +296,9 @@ struct node {
         ++get_large()[c];
     }
 
-    auto get(u32 i) const -> char {
+    auto get(freq_type i) const -> char_type {
         if (small) return small_char();
-        u32 n = 0;
+        freq_type n = 0;
         for (auto [c, p] : get_large()) {
             if (i < n + p) return c;
             n += p;
@@ -174,131 +314,77 @@ struct node {
         return *reinterpret_cast<const large_t*>(data);
     }
 
-    [[clang::always_inline]] auto small_char() const -> char {
-        return char(data & 0xff);
+    [[clang::always_inline]] auto small_char() const -> char_type {
+        return char_type(data & 0xff);
     }
 
-    [[clang::always_inline]] auto small_count() const -> u32 {
-        return u32(data >> 8);
+    [[clang::always_inline]] auto small_count() const -> freq_type {
+        return freq_type(data >> 8);
     }
 
-    [[clang::always_inline]] void inc_small_count(char c, u32 n = 1) {
-        data = (small_count() + n) << 8 | u32(c);
+    [[clang::always_inline]] void inc_small_count(char_type c, freq_type n = 1) {
+        data = (small_count() + n) << 8 | freq_type(c);
     }
 
-    [[clang::always_inline]] auto count() const -> u32 {
+    [[clang::always_inline]] auto count() const -> freq_type {
         if (small) return 1;
-        return u32(get_large().size());
+        return freq_type(get_large().size());
     }
 
-    auto total_count() const -> u32 {
+    auto total_count() const -> freq_type {
         if (small) return small_count();
         return rgs::fold_left(get_large().values(), 0u, [](auto a, auto& v) { return a + v; });
     }
 };
 
-template <>
-struct ser::Serialiser<node> {
-    static auto deserialise(auto& r) -> Result<node> {
-        auto count = Try(r.template read<u32>());
-        if (count == 0) return node();
-        if (count == 1) {
-            auto c = Try(r.template read<char>());
-            auto n = Try(r.template read<u32>());
-            return node(c, n);
-        }
-
-        return node(Try(r.template read<node::large_t>()));
-    }
-
-    static void serialise(auto& w, const node& n) {
-        auto count = n.count();
-        w << count;
-        if (count == 0) return;
-        if (count == 1) {
-            w <<  n.small_char() << n.small_count();
-            return;
-        }
-
-        w << n.get_large();
-    }
-};
-
-struct markov_chain {
-    using char_type = Character;
-    using text_type = std::basic_string_view<char_type>;
-    using string_type = std::basic_string<char_type>;
-    LIBBASE_SERIALISE(markov_chain, chain, order);
+struct markov_chain_builder {
+    using char_type = char;
+    using text_type = std::string_view;
+    LIBBASE_SERIALISE(markov_chain_builder, chain, order);
 
     /// Map from ngrams to [char, frequency] pairs.
-    using map_type = HashMap<__int128, node>;
+    using map_type = HashMap<markov_chain::ngram_type, node>;
 
     map_type chain;
     usz order;
-    usz seed;
-    std::mt19937 rng;
 
-    markov_chain(text_type text, usz order, usz _seed = std::random_device()()) : order(order), seed(_seed) {
-        Assert(order <= sizeof(__int128));
-        ProfileTimer _{"build"};
-        for (usz i = 0; i < text.size() - order; i++) {
-            __int128 v{};
-            std::memcpy(&v, text.data() + i, order);
-            chain[v].add(text[i + order]);
+    markov_chain_builder(text_type text, usz order) : order(order) {
+        Assert(order <= sizeof(markov_chain::ngram_type));
+        ProfileTimer timer{"build"};
+        usz end = text.size() - order;
+        {
+            // Hide/restore the cursor.
+            std::print("\033[?25l");
+            std::signal(SIGINT, [](int) {
+                std::print("\033[?25h");
+                std::fflush(stdout);
+                _Exit(1);
+            });
+
+            defer { std::print("\033[?25h"); };
+            for (usz i = 0; i < end; i++) {
+                if (i % 100000 == 0) {
+                    std::print(
+                        "\r{}/{} [{:3}%] ({} elapsed)                                                ",
+                        i,
+                        end,
+                        u32((double(i) / double(end)) * 100),
+                        chr::duration_cast<chr::seconds>(timer.elapsed())
+                    );
+                }
+
+                markov_chain::ngram_type v{};
+                std::memcpy(&v, text.data() + i, order);
+                chain[v].add(text[i + order]);
+            }
+            std::println();
         }
-        rng.seed(seed);
-    }
-
-    markov_chain(map_type chain, usz order, usz seed = std::random_device()())
-        : chain(std::move(chain)), order(order), seed(seed) {
-        rng.seed(seed);
-    }
-
-    string_type generate(usz length) {
-        __int128 ngram;
-        string_type result;
-        result.reserve(length);
-
-        // Pick an ngram that starts with a space.
-        for (;;) {
-            auto next = chain.begin();
-            std::advance(next, rng() % chain.size());
-            ngram = next->first;
-            if ((ngram & 0xff) != ' ') continue;
-            result.resize(order);
-            std::memcpy(result.data(), &ngram, order);
-            break;
-        }
-
-        // Append to it.
-        for (usz iterations = 0; iterations < length; iterations++) {
-            // Find the entry for the ngram in the chain; if there is no character
-            // that can follow this ngram, give up.
-            auto it = chain.find(ngram);
-            if (it == chain.end()) break;
-
-            // Otherwise, randomly pick a character; we need to take probabilities
-            // into account here, e.g. if 'Y' occurs 10 times more often after 'X'
-            // than 'Z', then we should pick 'Y' 10/11 times.
-            //
-            // To implement this, pick an ‘index’ randomly and then find the element
-            // at that ‘index’; e.g. if our map is {{'a', 10}, {'b', 5}, {'c', '20'}},
-            // then we pick an index I between 1 and 35, and the next character will
-            // be 'a' if I is in 0..<10, 'b' if it is in '10..<15', and 'c' otherwise.
-            auto count = it->second.total_count();
-            auto i = rng() % count;
-            result += it->second.get(u32(i));
-            std::memcpy(&ngram, result.data() + iterations + 1, order);
-        }
-
-        return result;
     }
 };
 
 using namespace command_line_options;
 using options = clopts< // clang-format off
-    multiple<option<"-f", "The input files", file<>>>,
-    flag<"--stdin", "Read input from stdin instead">,
+    positional<"input", "The input files", file<>>,
     option<"--length", "The maximum length of the output", int64_t>,
     option<"--lines", "How many lines to generate", int64_t>,
     option<"--order", "The order of the ngrams", int64_t>,
@@ -306,8 +392,9 @@ using options = clopts< // clang-format off
     option<"--min-line", "Ignore lines that are shorter than this", int64_t>,
     option<"--split", "Split output by regex">,
     option<"--save-chain", "Save the chain to a file">,
-    option<"--load-chain", "Load the chain from a file", file<std::vector<std::byte>>>,
+    option<"--chain", "Load the chain from a file">,
     flag<"--dump-input", "Print the processed text instead of generating output">,
+    flag<"--dump-chain", "Print the chain’s contents">,
     flag<"--print-seed", "Print the seed used for the random number generator">,
     flag<"--ascii", "Strip non-ascii characters">,
     flag<"--skip-preprocess", "Do not preprocess the input">,
@@ -357,12 +444,6 @@ void generate(options::optvals_type& opts, markov_chain& mc) {
     usz length = usz(opts.get<"--length">(100));
     usz lines = usz(opts.get<"--lines">(1));
 
-    // Save the chain.
-    if (auto* path = opts.get<"--save-chain">()) {
-        File::Write(*path, ser::Serialise<std::endian::native>(mc)).value();
-        return;
-    }
-
     // Print the seed.
     if (opts.get<"--print-seed">()) std::println(stderr, "Seed: {}", mc.seed);
 
@@ -390,7 +471,7 @@ void generate(options::optvals_type& opts, markov_chain& mc) {
     }
 }
 
-void build_chain_and_generate(options::optvals_type& opts, str input) {
+void build_chain(options::optvals_type& opts, fs::PathRef into, str input) {
     usz order = usz(opts.get<"--order">(6));
     String cleaned_up = opts.get<"--skip-preprocess">()
 #ifdef USE_32_BIT_CHAIN
@@ -407,54 +488,80 @@ void build_chain_and_generate(options::optvals_type& opts, str input) {
     }
 
     // Build the markov chain.
-    markov_chain mc(cleaned_up, order, usz(opts.get<"--seed">(std::random_device()())));
-    generate(opts, mc);
+    markov_chain_builder mc(cleaned_up, order);
+
+    // Save the chain.
+    header hdr{};
+    std::vector<markov_chain::saved_ngram> ngram_buffer;
+    std::vector<std::byte> freq_buffer;
+
+    {
+        ProfileTimer _{"serialising"};
+        Writer freq_writer{freq_buffer};
+
+        for (const auto& [k, v] : mc.chain) {
+            markov_chain::saved_ngram ngram{k, markov_chain::offset_type(freq_buffer.size())};
+            ngram_buffer.push_back(ngram);
+
+            markov_chain::freq_size_type count = v.count();
+            freq_writer.append_bytes(&count, sizeof(count));
+            if (v.small) {
+                markov_chain::freq_table_entry e{v.small_count(), v.small_char()};
+                freq_writer.append_bytes(&e, sizeof(e));
+            } else {
+                for (auto [c, freq] : v.get_large()) {
+                    markov_chain::freq_table_entry e{freq, c};
+                    freq_writer.append_bytes(&e, sizeof(e));
+                }
+            }
+        }
+
+        // Sort the engrams to enable binary search.
+        rgs::sort(ngram_buffer, rgs::less(), &markov_chain::saved_ngram::ngram);
+        hdr.num_ngrams = ngram_buffer.size();
+        hdr.order = u16(mc.order);
+    }
+
+    {
+        ProfileTimer _{"saving"};
+        auto f = File::Open(into, fs::OpenMode::Write).value();
+        f.write(ByteSpan{reinterpret_cast<const std::byte*>(&hdr), sizeof(header)}).value();
+        f.write(ByteSpan{reinterpret_cast<const std::byte*>(ngram_buffer.data()), ngram_buffer.size() * sizeof(markov_chain::saved_ngram)}).value();
+        f.write(freq_buffer).value();
+    }
+
+    std::fflush(stderr);
+    _Exit(0); // Avoid freeing everything we allocated.
 }
 
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "");
     auto opts = options::parse(argc, argv);
-    if (auto* chain_file = opts.get<"--load-chain">()) {
+    if (auto* chain_file = opts.get<"--chain">()) {
         Timer t{0ms};
-        markov_chain mc{ser::Deserialise<markov_chain, std::endian::native>(chain_file->contents).value()};
-        //markov_chain::map_type m;
-        //int x = 0;
-        //for (auto p : mc.chain) {
-        //    m.insert(std::move(p));
-        //    if (x++ == 1000) break;
-        //}
-        //
-        //std::println("{}", m);
-        //_Exit(41);
-        //std::unordered_map<int, int> x;
-        //for (const auto& [k, v] : mc.chain) x[v.size()]++;
-        //for (const auto& [k, v] : x) std::println("{} : {}", k, v);
-        //std::fflush(stdout);
-        //_Exit(42);
+        fs::FileContents chain_data;
 
-
-        std::println(stderr, "[deserialisation] {}", t.elapsed());
-        generate(opts, mc);
-        return 0;
-    }
-
-    if (opts.get<"--stdin">()) {
-        std::string input;
-        std::string line;
-        while (std::getline(std::cin, line)) {
-            input += line;
-            input += '\n';
+        {
+            ProfileTimer _{"load chain"};
+            chain_data = File::Read(*chain_file).value();
         }
 
-        build_chain_and_generate(opts, input);
+        markov_chain chain{chain_data.span(), usz(opts.get<"--seed">(std::random_device()()))};
+        if (opts.get<"--dump-chain">()) {
+            chain.dump();
+            return 0;
+        }
+
+        generate(opts, chain);
         return 0;
     }
 
-    auto fs = opts.get<"-f">();
-    if (fs.empty()) {
-        std::print(stderr, "{}", options::help());
+    auto* into = opts.get<"--save-chain">();
+    if (not into) {
+        std::println(stderr, "Either --save-chain or --chain is required");
         return 1;
     }
 
-    for (auto& input : fs) build_chain_and_generate(opts, input.contents);
+    auto input = opts.get<"input">();
+    build_chain(opts, *into, input->contents);
 }
